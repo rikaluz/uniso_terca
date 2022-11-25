23acb61c1a654741890abb48471ac04a00191b39
Redguitarock <83441754+Redguitarock@users.noreply.github.com>
2022-11-24 21:30:26 -0300

    Add files via upload

---
+++ 00108467-Pergunta.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peso ideal</title>
+ </head>
+
+ <body>
+ <p>Rafael Moreshi - RA: 00108467</p>
+
+<p>PERGUNTA</p>
+ Como as pessoas conseguem fortalecer a comunidade GIT?
+
+Compartilhando com seus projetos e tornando o ambiente uma referência para aprender, tirar dúvidas e até contribuir com a evolução
+de muitos projetos.
+
+
+ </body>
+
+</html>
