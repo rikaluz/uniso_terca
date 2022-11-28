0f7a943afb42ba9c92a8338ec6ef2798c9587cc8
Danilo Barreto <danilobarreto13@gmail.com>
2022-11-27 23:34:44 -0300

    3- Atualização

--- Grupo-04/BlazorWasm.FrontEnd/obj/Debug/net5.0/RazorDeclaration/Pages/Categoria/CadastrarCategoria.razor.g.cs
+++ Grupo-04/BlazorWasm.FrontEnd/obj/Debug/net5.0/RazorDeclaration/Pages/Categoria/CadastrarCategoria.razor.g.cs
@@ -13,98 +13,98 @@ namespace BlazorWasm.FrontEnd.Pages.Categoria
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 1 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 2 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 3 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 4 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 5 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 6 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 7 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 8 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 9 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasm.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 10 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasm.FrontEnd.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 11 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasm.FrontEnd.Helpers;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 12 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasm.FrontEnd.Repositorio;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 13 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasmServer.Client.Pages;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
+#line 14 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\_Imports.razor"
 using BlazorWasm.Compartilhado.Entidades;
 
 #line default
@@ -119,7 +119,7 @@ using BlazorWasm.Compartilhado.Entidades;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\danil\OneDrive\Área de Trabalho\Nova pasta\Grupo-04\BlazorWasm.FrontEnd\Pages\Categoria\CadastrarCategoria.razor"
+#line 9 "C:\Users\danil\OneDrive\Documentos\uniso_terca\Grupo-04\BlazorWasm.FrontEnd\Pages\Categoria\CadastrarCategoria.razor"
        
     private Categoria categoria = new Categoria();
 
